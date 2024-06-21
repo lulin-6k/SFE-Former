81232674f4f83335597c601c848eeec4d5f57846
Lu-Six <75114780+lulin-6k@users.noreply.github.com>
2024-06-21 10:06:08 +0800

    Create ESFF.py

---
+++ ESFF.py
@@ -0,0 +1,128 @@
+import torch
+from torch import nn
+
+class DilateAttention(nn.Module):
+
+    def __init__(self, head_dim, qk_scale=None, attn_drop=0, kernel_size=3, dilation=1):
+        super().__init__()
+        self.head_dim = head_dim
+        self.scale = qk_scale or head_dim ** -0.5
+        self.kernel_size=kernel_size
+        self.unfold = nn.Unfold(kernel_size=kernel_size, dilation=dilation, padding=dilation*(kernel_size-1)//2, stride=1)
+        self.attn_drop = nn.Dropout(attn_drop)
+
+    def forward(self,q,k,v):
+        B,d,H,W = q.shape
+        q = q.reshape([B, d//self.head_dim, self.head_dim, 1 ,H*W]).permute(0, 1, 4, 3, 2)   # B,h,N,1,d
+        k = self.unfold(k)
+        k = k.reshape([B, d//self.head_dim, self.head_dim, self.kernel_size*self.kernel_size, H*W]).permute(0, 1, 4, 2, 3)
+        attn = (q @ k) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        v = self.unfold(v).reshape([B, d//self.head_dim, self.head_dim, self.kernel_size*self.kernel_size, H*W]).permute(0, 1, 4, 3, 2)
+        x = (attn @ v).transpose(1, 2).reshape(B, H, W, d)
+        return x
+
+class LAE_Attention(nn.Module):
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None,
+                 attn_drop=0.,proj_drop=0., kernel_size=3, dilation=[1, 3]):
+        super().__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.dilation = dilation
+        self.kernel_size = kernel_size
+        self.scale = qk_scale or head_dim ** -0.5
+        self.num_dilation = len(dilation)
+        assert num_heads % self.num_dilation == 0, f"num_heads{num_heads} must be the times of num_dilation{self.num_dilation}!!"
+        self.qkv = nn.Conv2d(dim, dim * 3, 1, bias=qkv_bias)
+        self.dilate_attention = nn.ModuleList(
+            [DilateAttention(head_dim, qk_scale, attn_drop, kernel_size, dilation[i])
+             for i in range(self.num_dilation)])
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+        x = x.permute(0, 3, 1, 2) # (B,H,W,C)-->(B,C,H,W)
+        qkv = self.qkv(x)
+        qkv = qkv.detach().reshape(B, 3, self.num_dilation, C//self.num_dilation, H, W).permute(2, 1, 0, 3, 4, 5)
+        x = x.reshape(B, self.num_dilation, C//self.num_dilation, H, W).permute(1, 0, 3, 4, 2 )
+        for i in range(self.num_dilation):
+            x[i] = self.dilate_attention[i](qkv[i][0], qkv[i][1], qkv[i][2])
+        x = x.permute(1, 2, 3, 0, 4).reshape(B, H, W, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class ESFF(nn.Module):
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., window_size=2, alpha=0.5):
+        super().__init__()
+        head_dim = int(dim/num_heads)
+        self.dim = dim
+        self.l_heads = int(num_heads * alpha)
+        self.l_dim = self.l_heads * head_dim
+        self.h_heads = num_heads - self.l_heads
+        self.h_dim = self.h_heads * head_dim
+        self.ws = window_size
+        self.scale = round(qk_scale or head_dim ** -0.5, 3)
+        if self.l_heads > 0:
+            if self.ws != 1:
+                self.sr = nn.AvgPool2d(kernel_size=window_size, stride=window_size)
+            self.l_q = nn.Linear(self.dim, self.l_dim, bias=qkv_bias)
+            self.l_kv = nn.Linear(self.dim, self.l_dim * 2, bias=qkv_bias)
+            self.l_proj = nn.Linear(self.l_dim, self.l_dim)
+
+        self.LAE_Attention = LAE_Attention(dim=256)
+        self.classifiers = nn.Linear(75264, 2)
+
+    def GAE_Attention(self, x):
+        B, H, W, C = x.shape
+        q = self.l_q(x).reshape(B, H * W, self.l_heads, self.l_dim // self.l_heads).permute(0, 2, 1, 3)
+
+        if self.ws > 1:
+            x_ = x.permute(0, 3, 1, 2)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            kv = self.l_kv(x_).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.l_kv(x).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+
+        k, v = kv[0], kv[1]
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, H, W, self.l_dim)
+        x = self.l_proj(x)
+        return x
+
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+        x= x.reshape(4,7,H,W,C)
+        x = x.mean(dim=1)
+
+        LAE_out = self.LAE_Attention(x)
+        GAE_out = self.GAE_Attention(x)
+        b1,h1,w1,c1 = LAE_out.shape
+        b2, h2, w2, c2 = GAE_out.shape
+
+        x = torch.cat((LAE_out, GAE_out), dim=-1)
+        x = x.reshape(4,H,W,c1+c2)
+        x = x.view(x.size(0), -1)
+        x = self.classifiers(x)
+
+        return x
+
+
+
+
+if __name__ == '__main__':
+    # (B, num_token, C)   p:patch的尺寸, (H*W)/(p*p)=(H/p)*(W/p)=num_token 一般情况下,H=W,所以平方根: num_token**(-0.5)==(H/p)==(W/p)==每一行/列有多少个patch
+    # x = torch.randn(1, 196, 64)
+    # x = torch.randn(112,14,14,64)
+    x = torch.randn(28, 14, 14, 256)
+    Model = HiLo(dim=256, num_heads=8, window_size=2, alpha=0.5)
+    out = Model(x)
+    print(out)
