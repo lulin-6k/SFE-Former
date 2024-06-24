642432c9a15b1f6fedb0458c3ed9b12b7945fc92
Lu-Six <75114780+lulin-6k@users.noreply.github.com>
2024-06-24 17:22:53 +0800

    Update and rename LIM.py to SU.py

--- SU.py
+++ SU.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
 
-class LIM_G(nn.Module):
+class SU(nn.Module):
     def __init__(self, in_channel):
-        super(LIM_G, self).__init__()
+        super(SU, self).__init__()
         self.l_conv = nn.Sequential(
             nn.Conv2d(in_channel, in_channel, kernel_size=3, padding=1, stride=1),
             nn.Sigmoid()
@@ -12,10 +12,8 @@ class LIM_G(nn.Module):
     def forward(self, x, b, t):
         b_t, c, h, w = x.shape
         x = x.reshape(b, t, c, h, w)
-        x_avg = torch.sum(x, dim=1)  # 将所有特征相加
-        # 通过卷积获取特征图
+        x_avg = torch.sum(x, dim=1)
         att_map = self.l_conv(x_avg)
         att_map = att_map.unsqueeze(1).expand(x.shape)
         att_feature = (att_map * x).reshape(b_t, c, h, w)
         return att_feature
-
